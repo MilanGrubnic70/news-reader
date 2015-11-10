ffc1064a32fde862b4d1e31dc5de1e87e107f7c4
Milan Grubnić <MilanG@aol.com>
2015-11-09 23:07:28 -0600

    add n for next keypress

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var main = function() {
     $('.article').click(function() {
-        $('.article').removeClass('current');
+        $('.article').removeClass('current')
         $('.description').hide();
         $(this).addClass('current');
         $(this).children('.description').show();
+    });
+    $(document).keypress(function(event) {
+        if (event.which === 111) {
+            $('.current').children('.description').toggle();
+        }
     });
 };
 
